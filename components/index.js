361a4c961354799cec106117692071eae6af9722
masarufuruya <yannwennri2@gmail.com>
2018-05-16 17:00:44 +0900

    package.jsonにmainを設定

---
+++ components/index.js
@@ -0,0 +1,14 @@
+// NOTE: sample
+// import config from './config'
+
+// export * from './Buttons'
+// export * from './Row'
+// export * from './Base'
+// export * from './Text'
+// export * from './Nav'
+// export * from './Icons'
+// export * from './RowInput'
+// export * from './Input'
+// export * from './Pages'
+// export * from './Image'
+// export * from './Card'
