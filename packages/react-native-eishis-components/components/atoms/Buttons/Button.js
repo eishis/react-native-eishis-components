688ad9ef65c0fcee0497e99a78dc7571f600db50
UematsuYui <yuikoro12@gmail.com>
2018-06-20 16:54:22 +0900

    edit Button component

--- packages/react-native-eishis-components/components/atoms/Buttons/Button.js
+++ packages/react-native-eishis-components/components/atoms/Buttons/Button.js
@@ -1,39 +1,68 @@
 import React from 'react';
 import { TouchableOpacity, StyleSheet, Text } from 'react-native';
-
-// type Props = {
-//   style: any,
-//   children: Object,
-//   onPress: () => void,
-// }
+import { Colors } from '../../../styles';
 
 const Button = (props) => {
-  // const {
-  //   style,
-  //   onPress,
-  // } = props;
+  const buttonType = props.type || 'blue';
+  const { buttonStyle, textStyle } = styles;
+  const buttonColor = buttonColors[buttonType];
+  const {
+    onPress,
+    children,
+    style,
+    type,
+    disabled,
+  } = props;
+
   return (
     <TouchableOpacity
-      style={styles.button}
+      onPress={onPress}
+      style={[buttonStyle, { backgroundColor: disabled ? buttonColor.disabled : buttonColor.button }, style]}
+      disabled={disabled}
     >
-      <Text style={styles.text}>テスト</Text>
+      <Text style={[textStyle, { color: buttonColor.text }]} >{children}</Text>
     </TouchableOpacity>
-  );
+  ); 
 };
 
-export default Button;
+const buttonColors = {
+  blue: {
+    button: Colors.blue,
+    disabled: `${Colors.blue}50`,
+    text: Colors.white,
+  },
+  yellow: {
+    button: Colors.yellow,
+    disabled: `${Colors.yellow}50`,
+    text: Colors.white,
+  },
+  gray: {
+    button: Colors.gray,
+    disabled: `${Colors.gray}50`,
+    text: Colors.black,
+  },
+  red: {
+    button: Colors.red,
+    disabled: `${Colors.red}50`,
+    text: Colors.white,
+  },
+};
 
 const styles = StyleSheet.create({
-  button: {
-    width: 50,
-    height: 50,
-    borderStyle: 'solid',
-    borderColor: '#ccc',
-    borderWidth: 1,
+  buttonStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 12,
+    width: 240,
+    height: 40,
+    borderRadius: 80,
   },
-  text: {
-    color: '#333',
-    fontSize: 11,
+  textStyle: {
+    fontSize: 15,
+    textAlign: 'center',
+    fontFamily: "HiraKakuProN-W6",
   },
 });
 
+export default Button;
---
+++ packages/react-native-eishis-components/styles/index.js
@@ -0,0 +1,15 @@
+import { Platform } from 'react-native';
+
+export const Colors = {
+  blue: '#6EC4FF',
+  white: '#fff',
+  yellow: '#F9BF4B',
+  red: '#FF6969',
+  black: '#5D5D5E',
+  gray: '#dcdbda',
+  lightgray: '#999999',
+  bordergray: '#979797',
+  timetextgray: '#878181',
+  modalblack: 'rgba(120, 120, 118, 0.85)',
+  modalbg: 'rgba(0, 0, 0, 0.5)',
+};
