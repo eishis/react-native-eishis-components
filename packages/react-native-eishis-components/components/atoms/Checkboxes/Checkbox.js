bb1b2a467aa74d32d866c01b9672aac5e3a45d57
takashi yamakawa <yamakawa@eishis.com>
2018-09-06 17:31:18 +0900

    fixed styles

--- packages/react-native-eishis-components/components/atoms/Checkboxes/Checkbox.js
+++ packages/react-native-eishis-components/components/atoms/Checkboxes/Checkbox.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TouchableWithoutFeedback, StyleSheet, Text, Image, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, Image, View } from 'react-native';
 import { Colors } from '../../../styles';
 
 export default class Checkbox extends Component {
@@ -30,25 +30,18 @@ export default class Checkbox extends Component {
     const {
       container,
       checkIcon,
-      checkOffIcon, 
     } = styles;
 
+    const checkImage = this.state.isChecked ? require('../../../images/check_on.png') : require('../../../images/check_off.png');
+
     return (
-      <TouchableWithoutFeedback
+      <TouchableOpacity
         onPress={() => this.onClick()}
         style={container}
       >
-        {this.state.isChecked &&
-          <Image source={require('../../../images/check_on.png')} style={checkIcon} />
-        }
-        {!this.state.isChecked &&
-          <Image source={require('../../../images/check_off.png')} style={checkIcon} />
-        }
-        
-        {/* {this.render} */}
+        <Image source={checkImage} style={checkIcon} />
         {this.renderLeftItem()}
-        {/* {this.renderCenterItem()} */}
-      </TouchableWithoutFeedback>
+      </TouchableOpacity>
     ); 
   }
 };
@@ -64,14 +57,6 @@ const styles = StyleSheet.create({
   checkIcon: {
     width: 24,
     height: 24,
-    marginRight: 10,
-  },
-  checkOffIcon: {
-    width: 24,
-    height: 24,
-    borderRadius: 25,
-    borderWidth: 2,
-    borderColor: '#666',
     marginRight: 10,
   }
 });
