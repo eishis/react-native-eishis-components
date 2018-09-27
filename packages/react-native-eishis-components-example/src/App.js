240c68ebc575eac947f6511f55e524781c543a66
Yudai Shinnoki <y.shinnoki@gmail.com>
2018-09-27 17:33:20 +0900

    Fix openDrawer

--- packages/react-native-eishis-components-example/src/App.js
+++ packages/react-native-eishis-components-example/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
-import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
+import {
+  createDrawerNavigator,
+  createStackNavigator,
+  DrawerActions,
+} from 'react-navigation';
 import Buttons from './screens/Buttons';
 import Checkboxes from './screens/Checkboxes';
 
 const ButtonsStack = createStackNavigator({
   Buttons: {
     screen: Buttons,
-    navigationOptions: ({ navigation }) =>({
+    navigationOptions: ({ navigation }) => ({
       title: 'Buttons',
       headerLeft: (
-        <TouchableOpacity onPress={() => navigation.navigate('DrawerOpen')}>
+        <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
           <Text>Menu</Text>
         </TouchableOpacity>
       ),
@@ -18,13 +22,23 @@ const ButtonsStack = createStackNavigator({
   },
 });
 
-const CheckBoxesStack = createStackNavigator({
-  Checkboxes: { screen: Checkboxes },
+const CheckboxesStack = createStackNavigator({
+  Checkboxes: {
+    screen: Checkboxes,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Checkboxes',
+      headerLeft: (
+        <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.openDrawer())}>
+          <Text>Menu</Text>
+        </TouchableOpacity>
+      ),
+    }),
+  },
 });
 
 const AppNavigator = createDrawerNavigator({
-  ButtonsStack: { screen: ButtonsStack },
-  CheckBoxesStack: { screen: CheckBoxesStack },
+  Buttons: { screen: ButtonsStack },
+  Checkbox: { screen: CheckboxesStack },
 })
 
 export default AppNavigator;
