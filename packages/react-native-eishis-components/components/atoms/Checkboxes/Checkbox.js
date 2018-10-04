507cc9a8243d0de1404b92845861572cf27ef3d1
takashi yamakawa <yamakawa@eishis.com>
2018-10-04 16:47:49 +0900

    icon draw css

--- packages/react-native-eishis-components/components/atoms/Checkboxes/Checkbox.js
+++ packages/react-native-eishis-components/components/atoms/Checkboxes/Checkbox.js
@@ -31,6 +31,9 @@ export default class Checkbox extends Component {
       container,
       checkIcon,
       checkOffIcon, 
+      iconInner,
+      path1,
+      path2,
     } = styles;
 
     return (
@@ -39,10 +42,15 @@ export default class Checkbox extends Component {
         style={container}
       >
         {this.state.isChecked &&
-          <Image source={require('../../../images/icon-check-on.png')} style={checkIcon} />
+          <View style={checkIcon}>
+            <View style={iconInner}>
+              <View style={path1} />
+              <View style={path2} />
+            </View>
+          </View>
         }
         {!this.state.isChecked &&
-          <View style={checkOffIcon} />
+          <View style={checkOffIcon}></View>
         }
         {this.render}
         {this.renderLeftItem()}
@@ -54,16 +62,25 @@ export default class Checkbox extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
+    width: '80%',
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'flex-start',
+    marginBottom: 16,
+    marginTop: 16,
   },
   checkIcon: {
     width: 24,
     height: 24,
-    marginRight: 10,
+    marginRight: 16,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#f00',
+    backgroundColor: '#f00',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   checkOffIcon: {
     width: 24,
@@ -71,6 +88,29 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     borderWidth: 2,
     borderColor: '#666',
-    marginRight: 10,
+    marginRight: 16,
+  },
+  iconInner: {
+    width: 12,
+    height: 8,
+    position: 'relative',
+    transform: [{ rotate: '-45deg'}],
+    marginTop: -2,
+  },
+  path1: {
+    width: 3,
+    height: 8,
+    backgroundColor: '#fff',
+    position: 'absolute',
+    top: 0,
+    left: 0
+  },
+  path2: {
+    width: 12,
+    height: 3,
+    backgroundColor: '#fff',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
   }
 });
